a80cf4999f67819ed9db7f61ce4c3362da64573f
Raaj52 <mail2raaj52@gmail.com>
2020-07-27 13:16:55 +0530

    RMX1801: prebuilts: Add GCam GO
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

--- device.mk
+++ device.mk
@@ -446,3 +446,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    GCamGo
